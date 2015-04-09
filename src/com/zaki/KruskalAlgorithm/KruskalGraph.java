ae8d53fd2c9df2bd6f7ea1a55a89634a6a199e97
Marcin Zakidalski <Marcin_Zakidalski@epam.com>
2015-04-09 16:10:30 +0200

    Second modification

---
+++ src/com/zaki/KruskalAlgorithm/KruskalGraph.java
@@ -0,0 +1,12 @@
+package com.zaki.KruskalAlgorithm;
+
+import com.zaki.PrimAlgorithm.SimplePrimGraph;
+
+public class KruskalGraph extends SimplePrimGraph{
+
+	public KruskalGraph(int size) {
+		super(size);
+	}
+	
+
+}
